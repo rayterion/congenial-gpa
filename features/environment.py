2c26026b40076ce7639fa6e5093c7712df968be4
Túlio <kmaopop@gmail.com>
2026-05-03 18:00:51 -0300

    made the environment comply with the docker setup

--- features/environment.py
+++ features/environment.py
@@ -1,13 +1,10 @@
 from src.services.database_api import DatabaseAPI
-from apps.cli_app import cli_app
-from subprocess import Popen, PIPE, run
-from scripts import DevDatabase
 from support import CLIMockTerminal
+from scripts.dev_db import get_db_url, up_dev_db, down_dev_db
 
 def before_all(context):
-    context.dev_db = DevDatabase()
-    context.dev_db.run()
-    context.database_url = context.dev_db.get_db_url()
+    up_dev_db()
+    context.database_url = get_db_url()
 
     context.api_port = 8000
     context.db_api = DatabaseAPI(port=context.api_port, database_url=context.database_url)
@@ -17,7 +14,7 @@ def before_all(context):
     context.cli_terminal = CLIMockTerminal()
 
 def after_all(context):
-    context.dev_db.shutdown()
+    down_dev_db()
     context.db_api.shutdown()
 
 def after_scenario(context, scenario):
